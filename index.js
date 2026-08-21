f601b0fe0f4bea6b88d138414f195dbc2bb2840a
anas@gmail.com <iamazamkhan37@gmail.com>
2026-08-21 09:11:28 +0000

    updated

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ app.get('/', (req, res) => {
   res.send('Hello from Node.js app running in Docker!');
 });
 
-if (require.main === module) {
-  app.listen(3000, () => console.log('Server running on port 3000'));
-}
-
-module.exports = app;
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
