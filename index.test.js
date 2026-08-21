1aa30eea51711c43267cb59428e1626456e8f45b
anas@gmail.com <iamazamkhan37@gmail.com>
2026-08-21 07:24:01 +0000

    test file

---
+++ index.test.js
@@ -0,0 +1,16 @@
+const request = require('supertest');
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js app running in Docker!');
+});
+
+describe('GET /', () => {
+  it('should return greeting message', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toBe('Hello from Node.js app running in Docker!');
+  });
+});
+
